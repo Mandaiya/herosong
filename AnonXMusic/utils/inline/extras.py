e4f339bac45cf8118efdec5db2f9c1d20ae6d8cb
saavudaw <166626263+saavudaw@users.noreply.github.com>
2024-04-11 16:00:43 +0530

    Update extras.py

--- AnonXMusic/utils/inline/extras.py
+++ AnonXMusic/utils/inline/extras.py
@@ -7,7 +7,7 @@ def botplaylist_markup(_):
     buttons = [
         [
             InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="NOI NOI"),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
