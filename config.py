ae7b1044364ea3746737984d33ebe56672eaca41
natha <108113193+Mandaiya@users.noreply.github.com>
2024-05-03 21:46:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/saavudaw/insonehero",
+    "https://github.com/Mandaiya/herosong",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
